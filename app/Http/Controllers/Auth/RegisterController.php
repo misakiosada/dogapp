6725ed486fc83977409c7a36384e9d475bf60bc9
MO <mo@MOs-MacBook-Air.local>
2021-09-05 22:12:25 +1000

    acreated amigration to add cloums to user table and updated RegisterController & User.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,6 +52,9 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'dog_name' => ['required', 'string'],
+            'address' => ['required', 'string'],
+            'image' => ['required', 'string'],
         ]);
     }
 
@@ -67,6 +70,9 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'dog_name' => $data['dog_name'],
+            'address' => $data['address'],
+            'image' => $data['image'],
         ]);
     }
 }
