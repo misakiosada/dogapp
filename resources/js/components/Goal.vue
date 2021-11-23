7be9666e5779da42b4c7de0778d3272ef3a754c1
MO <mo@MOs-MacBook-Air.local>
2021-11-23 22:54:23 +1100

    goal.vue

--- resources/js/components/Goal.vue
+++ resources/js/components/Goal.vue
@@ -9,9 +9,7 @@
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalLabel">New Goal Name</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
+
                     </div>
                     <div class="modal-body">
                         <input v-model="title" class="form-control">
