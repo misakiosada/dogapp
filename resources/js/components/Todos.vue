d068a30d8bbd224fc1810249e5eced900cb3da4b
MO <mo@MOs-MacBook-Air.local>
2021-11-23 21:55:18 +1100

    todo.vue

--- resources/js/components/Todos.vue
+++ resources/js/components/Todos.vue
@@ -1,25 +1,6 @@
 <template>
     <div class="container-fruid" style="overflow-y: scroll;">
 
-        <div class="modal fade" :id="'todoModal'+goalId" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">New Todo Content</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                    </div>
-                    <div class="modal-body">
-                        <input v-model="content" class="form-control">
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal" v-on:click="addNewTodo">Add</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
         <div v-for="(todo, key, index) in todos" :key=index>
             <div class="d-flex justify-content-center m-3">
                 <div class="card" style="width: 18rem;" v-if="todo.done == 0">
